e21ca5ffd87aa1de76d251f409f375868d1873f3
juskin <juskin.jia@gmail.com>
2014-03-14 11:56:25 +0800

    修改@string 变量

--- ex07_1_button/src/com/eoeAndroid/button/ActivityButton.java
+++ ex07_1_button/src/com/eoeAndroid/button/ActivityButton.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 //import com.eoeAndroid.button.R;
 
 public class ActivityButton extends Activity {
